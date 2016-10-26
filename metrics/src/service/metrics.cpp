0774ab4fe331814746a0d2861c3c3250a096e981
antmat <antmat@me.com>
2016-10-26 21:09:30 +0300

    fix(): prevent GCC complains about shadowed member

--- metrics/src/service/metrics.cpp
+++ metrics/src/service/metrics.cpp
@@ -21,10 +21,10 @@ namespace service {
 
 metrics_t::metrics_t(context_t& context,
                      asio::io_service& asio,
-                     const std::string& name,
+                     const std::string& _name,
                      const dynamic_t& args) :
-    api::service_t(context, asio, name, args),
-    dispatch<io::metrics_tag>(name),
+    api::service_t(context, asio, _name, args),
+    dispatch<io::metrics_tag>(_name),
     hub(context.metrics_hub()),
     sender()
 {
@@ -44,41 +44,41 @@ auto metrics_t::metrics() const -> dynamic_t {
     dynamic_t::object_t result;
 
     for (const auto& item : hub.counters<std::int64_t>()) {
-        const auto& name = std::get<0>(item).name();
+        const auto& _name = std::get<0>(item).name();
         const auto& counter = std::get<1>(item);
 
-        result[name] = counter.get()->load();
+        result[_name] = counter.get()->load();
     }
 
     for (const auto& item : hub.meters()) {
-        const auto& name = std::get<0>(item).name();
+        const auto& _name = std::get<0>(item).name();
         const auto& meter = std::get<1>(item);
 
-        result[name + ".count"] = meter.get()->count();
-        result[name + ".m01rate"] = meter.get()->m01rate();
-        result[name + ".m05rate"] = meter.get()->m05rate();
-        result[name + ".m15rate"] = meter.get()->m15rate();
+        result[_name + ".count"] = meter.get()->count();
+        result[_name + ".m01rate"] = meter.get()->m01rate();
+        result[_name + ".m05rate"] = meter.get()->m05rate();
+        result[_name + ".m15rate"] = meter.get()->m15rate();
     }
 
     for (const auto& item : hub.timers()) {
-        const auto& name = std::get<0>(item).name();
+        const auto& _name = std::get<0>(item).name();
         const auto& timer = std::get<1>(item);
 
-        result[name + ".count"] = timer.get()->count();
-        result[name + ".m01rate"] = timer.get()->m01rate();
-        result[name + ".m05rate"] = timer.get()->m05rate();
-        result[name + ".m15rate"] = timer.get()->m15rate();
+        result[_name + ".count"] = timer.get()->count();
+        result[_name + ".m01rate"] = timer.get()->m01rate();
+        result[_name + ".m05rate"] = timer.get()->m05rate();
+        result[_name + ".m15rate"] = timer.get()->m15rate();
 
         const auto snapshot = timer->snapshot();
 
-        result[name + ".mean"] = snapshot.mean() / 1e6;
-        result[name + ".stddev"] = snapshot.stddev() / 1e6;
-        result[name + ".p50"] = snapshot.median() / 1e6;
-        result[name + ".p75"] = snapshot.p75() / 1e6;
-        result[name + ".p90"] = snapshot.p90() / 1e6;
-        result[name + ".p95"] = snapshot.p95() / 1e6;
-        result[name + ".p98"] = snapshot.p98() / 1e6;
-        result[name + ".p99"] = snapshot.p99() / 1e6;
+        result[_name + ".mean"] = snapshot.mean() / 1e6;
+        result[_name + ".stddev"] = snapshot.stddev() / 1e6;
+        result[_name + ".p50"] = snapshot.median() / 1e6;
+        result[_name + ".p75"] = snapshot.p75() / 1e6;
+        result[_name + ".p90"] = snapshot.p90() / 1e6;
+        result[_name + ".p95"] = snapshot.p95() / 1e6;
+        result[_name + ".p98"] = snapshot.p98() / 1e6;
+        result[_name + ".p99"] = snapshot.p99() / 1e6;
     }
 
     return result;
