7d64e7dd9b9bfa39186dbae22ab4c18ae8edc382
antmat <antmat@me.com>
2017-08-01 12:31:00 +0300

    fix: invalid name exception in vicodyn, move out invocation param.

--- vicodyn/src/vicodyn/pool.cpp
+++ vicodyn/src/vicodyn/pool.cpp
@@ -1,4 +1,5 @@
 #include "cocaine/vicodyn/pool.hpp"
+#include "cocaine/vicodyn/invocation.hpp"
 #include "cocaine/format/peer.hpp"
 
 #include <cocaine/context.hpp>
@@ -48,14 +49,12 @@ pool_t::pool_t(context_t& _context, asio::io_service& _io_loop, const std::strin
 
 pool_t::~pool_t() = default;
 
-auto pool_t::invoke(const message_t& message, const io::graph_node_t& protocol, stream_ptr_t backward_stream)
-    -> stream_ptr_t
-{
+auto pool_t::invoke(invocation_t invocation) -> stream_ptr_t {
     return peers.apply([&](peers_t& peers){
-        std::shared_ptr<peer_t> peer = balancer->choose_peer(message, peers);
+        std::shared_ptr<peer_t> peer = balancer->choose_peer(invocation.message(), peers);
         try {
             COCAINE_LOG_DEBUG(logger, "processing invocation via {}", peer);
-            return peer->invoke(message, protocol, backward_stream);
+            return peer->invoke(std::move(invocation));
         } catch(std::system_error& e) {
             COCAINE_LOG_WARNING(logger, "peer errored: {}, peer: {}", error::to_string(e), peer);
             peer->freeze(freeze_time);
